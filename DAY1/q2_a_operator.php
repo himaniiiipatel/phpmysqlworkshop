03f38fda47b768596c3e52300a3d23ff9db93387
himaniiiipatel <ranpariyahimani@gmail.com>
2020-10-02 00:45:44 +0530

    First commit

---
+++ DAY1/q2_a_operator.php
@@ -0,0 +1,15 @@
+<?php
+$num1=6;
+$num2=2;
+$c=$num1+$num2;
+$d=$num1-$num2;
+$e=$num1*$num2;
+$f=$num1/$num2;
+echo "addition is $c <br>";
+
+echo "subtraction is $d <br>";
+
+echo "Multiplication is $e <br>";
+
+echo " Division is $f";
+?>
